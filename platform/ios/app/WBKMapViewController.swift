f17964ba780d11017c4a2eafdbf245e4d0f84746
William Kamp <manimaul@gmail.com>
2016-12-18 20:35:11 -0800

    swift map view / example

---
+++ platform/ios/app/WBKMapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WBKMapViewController.swift
+//  ios
+//
+//  Created by William Kamp on 12/15/16.
+//  Copyright © 2016 Mapbox. All rights reserved.
+//
+
+import UIKit
+import Mapbox
+
+class WBKMapViewController: UIViewController {
+    @IBOutlet weak var mapView: MGLMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
